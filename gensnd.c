9b7f8590166031abfa2d0d632dccc82264823d5b
MeepingGale <nickowk1@gmail.com>
2020-09-07 06:23:54 -0500

    skeleton code

--- gensnd.c
+++ gensnd.c
@@ -1,5 +1,46 @@
 #include <stdio.h>
 
-int main2() {
-  return 0;
+int getChar(char ch) {
+  switch (ch) {
+  case '1':
+    break;
+  case '2':
+    break;
+  case '3':
+    break;
+  case '4':
+    break;
+  case '5':
+    break;
+  case '6':
+    break;
+  case '7':
+    break;
+  case '8':
+    break;
+  case '9':
+    break;
+  case 'a':
+  case 'A':
+    break;
+  case 'b':
+  case 'B':
+    break;
+  case 'c':
+  case 'C':
+    break;
+  case 'd':
+  case 'D':
+    break;
+  case '*':
+   break;
+  case '0':
+    break;
+  case '#':
+    break;
+  default:
+    printf("Not in the thing\n");
+    return 0;
+  }
+  return 1;
 }
--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include "gensnd.c"
+
+int frequency;
+int sample_rate;
+int duration;
 
 int main(int agrc, char * agrv[]) {
-	return 0;
+
+  printf("Enter a frequency: \n");
+  scanf("%d", &frequency);
+  printf("Enter a sample rate: \n");
+  scanf("%d", &sample_rate);
+  printf("Enter the duration: \n");
+  scanf("%d", &duration);
+
+  printf("Enter a character: \n");
+  char ch;
+  scanf("%s", &ch);
+  getChar(ch);
+  
+  return 0;
 }
