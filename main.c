8125b232250a2fac0e9e1e365d30a6475ddac79f
Nicholas <NicholasOng@Nicholas-2.local>
2020-09-09 17:25:09 +0800

    changes

--- main.c
+++ main.c
@@ -7,11 +7,17 @@ float duration;
 
 int main(int agrc, char * agrv[]) {
 
+    char temp;
+    
     printf("Enter a frequency: \n");
     scanf("%f", &frequency);
     
+    if(scanf("%f%c", &frequency, &temp) != 2 || temp != '\n')
+        printf("failure\n");
+    
     printf("Enter a sample rate: \n");
     scanf("%f", &sample_rate);
+    
     printf("Enter the duration: \n");
     scanf("%f", &duration);
     
--- main2.c
+++ main2.c
@@ -2,6 +2,18 @@
 #include <string.h>
 #include "gensnd.c"
 
+// Returns 1 if it's invalid, 0 otherwise
+int check_validity(char* str) {
+    char* key_pad = "0123456789*#";
+
+    for(int i = 0; i < strlen(str); i++)
+        for(int j = 0; j < strlen(key_pad); j++)
+            if(key_pad[j] != str[i])
+                return 1;
+            
+    return 0;
+}
+
 int main() {
     // Allocate size of 10
     char* str = (char*)malloc(10);
@@ -9,17 +21,21 @@ int main() {
     printf("Enter a character: \n");
     scanf("%s", str);
 
-    // Repeat until the user inputs 10 digits
+    // Repeat until the user inputs 10 digits & checks validity
     while(strlen(str) != 10) {
         printf("The length is not 10! Enter a character: \n");
         scanf("%s", str);
     }
     
+    if(check_validity(str))
+        printf("error");
+    
     // Loop 
-    for(int i = 0; i < strlen(str)	; i++) {
-        phonePad(str[i]);
-        makeSilence(8000, 0.25);
-    }
+//    for(int i = 0; i < strlen(str); i++) {
+//        phonePad(str[i]);
+//        makeSilence(8000, 0.25);
+//    }
     
 	return 0;
 }
+
