47cf4acc5bc697705f60a5aa9aa8879da41f76ea
Brendan <brend98@iastate.edu>
2020-09-29 08:43:55 -0500

    main1b skeleton

--- main1b.c
+++ main1b.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "gensnd.c"
 
-int main(int argc, char * argv[]) {
+int main(int argc, char **argv) {
     // This is to test outputfile function
     // sound *hello = gensine2(450, 9000, 2);
     // FILE *file = fopen("test", "w");
