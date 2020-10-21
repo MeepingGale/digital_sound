05041f58f21c37e6dd0a7dd563a0d8ea8b8f42f4
Brendan <brend98@iastate.edu>
2020-10-21 05:48:24 -0500

    added the mix function in process.c

--- process.c
+++ process.c
@@ -4,7 +4,27 @@
 #include "process.h"
 
 sound* mix(sound *s[],float w[], int c){
-  
+  int longestLength = 0;
+  for(int i = 0; i < c; i++){
+    if(s[i]->length > longestLength)
+      longestLength = s[i]->length;
+  }
+  if(longestLength == 0){
+    return NULL;
+  }
+  sound *mix = (sound*)malloc(sizeof(sound));
+  mix->rate = sample_rate;
+  mix->length = sample_rate * duration;
+  mix->samples = (float *)malloc(sizeof(float)*longestLength);
+  for(int i = 0; i < longestLength; i++) {
+    mix->samples[i] = 0;
+  }
+  for(int i = 0; i < c; i++) {
+    for (int j = 0; j < s[i]->length; j++) {
+      mix->samples[j] += s[i]->samples[j]*w[i];
+    }
+  }
+  return mix;
 }
 
 sound* modulate(sound *s1, sound *s2){
