0f47043c35e3af8347b09166914acb75bdcceda0
Brendan Yeong <brendanyeong@mac-air-048.local>
2020-11-09 18:04:15 +0800

    soundio done

--- part2/soundio.cpp
+++ part2/soundio.cpp
@@ -5,15 +5,12 @@
 
 using namespace std;
 
-class SoundIO {
- public:
-    static void OutputSound(SoundSamples *samples, string filename) {
+     void SoundIO::OutputSound(SoundSamples *samples, string filename){
         ofstream MyFile(filename);
         
-        for(int i = 0; i < samples->sample_len; i++)
-            MyFile << *(samples->sequence_of_samples + i) << endl;
+        for(int i = 0; i < samples->getSampleRate(); i++)
+            MyFile << samples->sequence_of_samples[i] << endl;
 
         // Close the file
         MyFile.close();
     }
-};
--- part2/soundio.h
+++ part2/soundio.h
@@ -7,7 +7,9 @@
 
 using namespace std;
 
+class SoundIO {
+ public:
+    static void OutputSound(SoundSamples *samples, string filename);
 
-void OutputSound(SoundSamples *samples, string filename);
-
+};
 #endif
